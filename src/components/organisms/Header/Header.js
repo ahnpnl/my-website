f5f30a5a6de02d256786b8bf5259a1fe8ef8d5ca
AhnpGit <anhpnnd@gmail.com>
2017-07-10 16:22:48 +0200

    Move some temporary files to temp folder

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -2,6 +2,7 @@
 
 import React from "react"
 import PropTypes from 'prop-types'
+
 import Icon from '../../atoms/Icon/Icon'
 import type { DeviceInfo } from '../../../utils/types'
 
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  webpackConfig: require('./config/webpack.config.dev'),
-  components: 'src/components/atoms/**/*.js'
-}
+'use strict'
 
+const path = require('path')
+
+module.exports = {
+  webpackConfig: Object.assign({},
+    require('./config/webpack.config.dev')
+  ),
+  compilerConfig: {
+    transforms: {
+      // The compiler used by the style guide cannot handle tagged template strings.
+      // Since all relevant browsers these days have implemented tagged template strings, we disable this option.
+      templateString: false,
+    },
+  },
+  components: 'src/components/**/*.js',
+}
